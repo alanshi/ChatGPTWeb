a3fae1acfbd43b0f613b2a51169d73841832b861
alan <alan.shi86@gmail.com>
2023-03-04 23:52:03 +0800

    update author

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,8 +15,8 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
+      avatar: 'https://raw.githubusercontent.com/alanshi/ChatGPTWeb/25e5de2490db005793aec7205c5cac08529b9bdd/src/assets/avatar.jpg',
+      name: 'AnchorPoint',
       description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
