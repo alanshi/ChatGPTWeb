9ce407a2b299965adceaeda203847b6f6dd38c34
alan <alan.shi86@gmail.com>
2023-03-16 10:38:05 +0800

    update log

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -12,22 +12,19 @@ const userInfo = computed(() => userStore.userInfo)
 
 <template>
   <div class="flex items-center">
-    <div class="overflow-hidden" >
+    <div class="overflow-hidden">
       <template v-if="isString(userInfo.avatar) && userInfo.avatar.length > 0">
-        <NAvatar
-        :size="96"
-          :src="userInfo.avatar"
-          :fallback-src="defaultAvatar"
-        />
+        <NAvatar :size="96" :src="userInfo.avatar" :fallback-src="defaultAvatar" />
       </template>
       <template v-else>
-        <NAvatar      
-          :size="96"
-          :src="defaultAvatar" />
+        <NAvatar :size="96" :src="defaultAvatar" />
       </template>
     </div>
-    <div class="ml-2">
 
+    <div class="ml-2">
+      <h1 class="overflow-hidden font-bold text-md text-ellipsis whitespace-nowrap">
+        {{ userInfo.name ?? '睿畜科技' }}
+      </h1>
     </div>
   </div>
 </template>
